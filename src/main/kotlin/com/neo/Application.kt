5faf74b31a4fd06787f485d518f80fbb680455ab
Jaeuk Oh <wooko5@naver.com>
2024-07-10 23:41:29 +0900

    init project

---
+++ src/main/kotlin/com/neo/Application.kt
@@ -0,0 +1,12 @@
+package com.neo
+
+import com.neo.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
